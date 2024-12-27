c3545a30d82d4ba8332b2eb518430bdf7f0f8070
Zuhair Emaz <zuhair.emaz@gmail.com>
2024-12-26 19:37:55 -0700

    Added title and created the sketch canvas
    Added adaptable size to the canvas to make it look good regardless of zoom level

---
+++ script.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector(".canvas");
+
+let canvasDimension = 16;
+
+function createCanvas() {
+  for (let i = 0; i < canvasDimension; i++) {  // Use 'let' to properly declare 'i'
+    const row = document.createElement("div");
+    row.classList.add("row");
+    canvas.appendChild(row);
+    for (let x = 0; x < canvasDimension; x++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        //square.textContent = "s";  
+        row.appendChild(square);
+    }
+  }
+}
+
+createCanvas();
